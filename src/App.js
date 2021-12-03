aade059cf9ef26da7c549b0cb218be4b58f139c1
Seb <you@example.com>
2021-12-03 14:53:38 -0500

    Update boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import List from './pages/list/List';
+import Login from './pages/login/Login';
+import SpotifyAuth from './pages/spotify-auth/SpotifyAuth';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<List />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/spotify-auth" element={<SpotifyAuth />} />
+      <Route path="/spotify-callback" element={<SpotifyAuth />} />
+    </Routes>
+  )
 }
 
 export default App;
